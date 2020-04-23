887e022e07059c8141ba359f81649ff62e319e10
johannes karoff <johannes@karoff.net>
2020-04-23 09:37:18 +0200

    null instead of default result

--- lib/src/metadata_fetch_base.dart
+++ lib/src/metadata_fetch_base.dart
@@ -13,23 +13,15 @@ Future<Metadata> extract(String url) async {
     return null;
   }
 
-  /// Sane defaults; Always return the Domain name as the [title], and a [description] for a given [url]
-  final default_output = Metadata();
-  default_output.title = getDomain(url);
-  default_output.description = url;
-
   // Make our network call
   final response = await http.get(url);
   final document = responseToDocument(response);
 
   if (document == null) {
-    return default_output;
+    return null;
   }
 
   final data = _extractMetadata(document);
-  if (data == null) {
-    return default_output;
-  }
 
   return data;
 }
